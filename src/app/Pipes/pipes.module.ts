05b7f692994a214f736885c05aa409884e35ffd8
EGonza <xetchegaraygonzalo@gmail.com>
2019-09-09 17:17:58 -0300

    Agregando pipe

--- src/app/Pipes/pipes.module.ts
+++ src/app/Pipes/pipes.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconsCategoriaPipe } from './icons-categoria.pipe';
 
-
-
 @NgModule({
   declarations: [
     IconsCategoriaPipe
