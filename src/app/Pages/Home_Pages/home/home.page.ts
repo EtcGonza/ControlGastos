9d0750e27d1bfff5e7c2dccc82054cf5fb20cf6b
EGonza <xetchegaraygonzalo@gmail.com>
2019-10-23 11:40:03 -0300

    Cree un modelo para la billetera

--- src/app/Pages/Home_Pages/home/home.page.ts
+++ src/app/Pages/Home_Pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MiBilleteraService } from '../../../Services/mi-billetera.service';
 
 @Component({
@@ -6,8 +6,11 @@ import { MiBilleteraService } from '../../../Services/mi-billetera.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor(private MiBilleteraService: MiBilleteraService) {}
 
+  ngOnInit() {
+    
+  }
 }
