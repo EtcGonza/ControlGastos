638679d1dcfab69377c9fa41de34855add7111c1
EGonza <xetchegaraygonzalo@gmail.com>
2019-10-23 11:26:13 -0300

    Agregue funciones basicas a mibilletera

--- src/app/Services/mi-billetera.service.ts
+++ src/app/Services/mi-billetera.service.ts
@@ -5,5 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class MiBilleteraService {
 
-  constructor() { }
+  private miBilletera = 0; // Corresponde al dinero actual que tengo.
+  private gastado = 0;     // Corresponde al dinero que gaste.
+  private sueldo = 0;      // Corresponde al dinero con el que empece el mes.
+
+  constructor() {}
+
+  // Funcion para agregar el sueldo iniciar del mes.
+  iniciarSueldo(monto: number) {
+  this.sueldo = monto;
+  this.miBilletera = monto;
+  }
+
+  sumarGasto(monto: number) {
+  this.gastado = this.gastado + monto;
+  this.miBilletera = this.miBilletera - monto;
+  }
+
+  sumarMiBilletera(monto: number) {
+  this.miBilletera = this.miBilletera + monto;
+  }
 }
